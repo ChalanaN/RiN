88adfbddbb13035c0c64e145f00ed48841aeaf68
Chalana Nethsara <wchalanaw@gmail.com>
2022-03-13 20:23:51 +0530

    Basic example 👨‍💻

---
+++ examples/basic/compile.js
@@ -0,0 +1,10 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import RiN from "../../lib/index.js"
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+await RiN(resolve(__dirname, "src"), "all", "default", {
+    outDir: resolve(__dirname, "dist"),
+    title: "RiN"
+})
